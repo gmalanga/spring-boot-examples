37b80fbcb8146dbcd9f3a8d077a3711d22554cd3
Giuseppe Malanga <giuseppe.malanga@gmail.com>
2024-07-27 14:46:44 +0100

    Initial draft of the project

---
+++ kafka-replying/src/main/java/com/example/kafka_replying/KafkaReplyingApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafka_replying;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaReplyingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaReplyingApplication.class, args);
+	}
+
+}
