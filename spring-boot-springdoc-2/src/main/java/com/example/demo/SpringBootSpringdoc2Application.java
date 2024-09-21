fb5218a63226c552e89faf023cbefe25c4b6ebbb
Giuseppe Malanga <giuseppe.malanga@gmail.com>
2024-09-21 18:15:20 +0100

    Added the spring-boot-springdoc-2 example

---
+++ spring-boot-springdoc-2/src/main/java/com/example/demo/SpringBootSpringdoc2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSpringdoc2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSpringdoc2Application.class, args);
+	}
+
+}
